b74e91dd3946fa96bbf21c772cece1d44e9e02c9
Nick <nikheal25@gmail.com>
2020-05-31 21:02:38 +1000

    Route initialise

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const post = require("./routes/api/posts");
+app.use("/api/posts", post);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on ${port} port!`));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//GET Post
+
+router.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
+//GET post
+
+//DELETE
+
+module.exports = router;
