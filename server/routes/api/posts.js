de75d368abbf8fc2b101ab7610a63e380fa38aae
Nick <nikheal25@gmail.com>
2020-05-31 21:56:11 +1000

    Post request added

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const mongodb = require("mongodb");
-
+const packageModel = require("../../models/packages");
 const router = express.Router();
 
 //GET Post
@@ -8,7 +8,36 @@ router.get("/", (req, res) => {
   res.send("GET request to the homepage");
 });
 
-//GET post
+//POST
+router.post("/", (req, res, next) => {
+  console.log(
+    `-----------------------  ${req}----------------------------------------------------`
+  );
+
+  const package = new packageModel({
+    packageId: "",
+    title: "",
+    subtitle: "",
+    profitablity: "",
+    amountLow: "",
+    amountHigh: "",
+    eligibilityCriteria: "",
+    universityCriteria: "",
+    universityName: "",
+    areaCriteria: "",
+    area: "",
+    timePeriodStart: "",
+    timePeriodEnd: "",
+    linkToMoreInfo: "",
+  });
+
+  package
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 //DELETE
 
