b54c99dc8a8186d962ddd2975c9ed15b02053549
Nick <nikheal25@gmail.com>
2020-06-10 16:51:50 +1000

    added 2 apis

--- server/apis/JoobleAPI.js
+++ server/apis/JoobleAPI.js
@@ -5,18 +5,33 @@ const axios = require("axios");
 const url = "https://jooble.org/api/";
 const key = "c403dcc5-77c9-40c7-b7af-df80733420f9";
 
-const params = {
-  keywords: "Web Developer",
-  location: "Melbourne",
-  radius: "50",
-  page: "1",
-};
+const findJobs = (
+  location = "Australia",
+  keyword = "Web Developer",
+  radius = "50",
+  page = "1"
+) => {
+  // const params = {
+  //     keywords: "Web Developer",
+  //     location: "Melbourne",
+  //     radius: "50",
+  //     page: "1",
+  // };
 
-axios
-  .post(url + key, params)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  const params = {
+    keywords: keyword,
+    location: location,
+    radius: radius,
+    page: page,
+  };
+
+  axios
+    .post(url + key, params)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+module.exports = findJobs;
